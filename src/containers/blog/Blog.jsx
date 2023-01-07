bb8414c83b9db2b7aae9f601a65d9b63444ccc9a
Gautham-22 <gautham22poy@gmail.com>
2023-01-07 14:32:57 +0530

    done articles & blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import "./blog.css";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3_blog section_padding' id='blog'>
+      <div className="gpt3_blog-heading">
+        <h1 className="gradient_text">
+          A lot is happening,
+          We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3_blog-container">
+        <div className="gpt3_blog-container_groupA">
+          <Article imgUrl={blog01} date="Dec 22, 2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        <div className="gpt3_blog-container_groupB">
+          <Article imgUrl={blog02} date="Dec 22, 2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog03} date="Dec 22, 2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog04} date="Dec 22, 2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={blog05} date="Dec 22, 2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+      </div>
+    </div>
   )
 }
 
